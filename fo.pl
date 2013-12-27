c9a8a8dde7ce48bcacb90a0b083c810759381816
Styopa Semenukha <semenukha@gmail.com>
2013-12-27 14:21:15 -0500

    Added basic sanity checks

--- fo.pl
+++ fo.pl
@@ -1,1 +1,25 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+$\ = "\n";
+
+if ($#ARGV < 1) {
+	my $num = $#ARGV + 1;
+	die "Too little arguments: $num\n" .
+		"USAGE: $0 PASSWORD PASSWORD..."
+}
+
+my $len = length($ARGV[0]);
+foreach my $passwd (@ARGV) {
+	if ( length($passwd) != $len ) {
+		die 'Passwords are of different length: ' .
+			"'${ARGV[0]}' and '$passwd'";
+	}
+}
+
+my @passwords = map(lc, @ARGV);
+
+for (0..$#ARGV) {
+	
+}
