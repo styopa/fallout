bc80c494cd71cda8d2a7be78d1656d31a714ed33
Styopa Semenukha <semenukha@gmail.com>
2013-12-27 16:15:16 -0500

    Added input reading

--- fo.pl
+++ fo.pl
@@ -10,15 +10,21 @@ if ($#ARGV < 1) {
 }
 
 my $len = length($ARGV[0]);
-foreach my $passwd (@ARGV) {
-	if ( length($passwd) != $len ) {
+foreach my $arg (@ARGV) {
+	if ( length($arg) != $len ) {
 		die 'Passwords are of different length: ' .
 			"'${ARGV[0]}' and '$arg'\n";
 	}
 }
 
-my @passwords = map(lc, @ARGV);
+# make 'em lowercase and convert to hash keys
+my %passwords = map {$_ => 0} map(lc, @ARGV);
 
-for (0..$#ARGV) {
-	
+my ($passwd, $correct);
+for (0 .. $#ARGV) {
+	print "Enter password and number of correct letters.\n> ";
+	<STDIN>;
+	($passwd, $correct) = split /\W+/;
+	$passwd = lc($passwd);
+	$correct = int($correct);
 }
