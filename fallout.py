041f2edcaa05b8161d038964555c14782db5d876
Styopa Semenukha <semenukha@gmail.com>
2015-11-12 21:21:58 -0500

    Drew the rest of the owl

--- fallout.py
+++ fallout.py
@@ -2,7 +2,7 @@
 # helps hack terminals in Fallout 3 and 4
 # copyright 2015, Styopa Semenukha, GPL v3+
 from __future__ import print_function
-import sys
+import sys, re
 
 def hamming(op1, op2):
     i = 0
@@ -11,16 +11,39 @@ def hamming(op1, op2):
             i += 1
     return i
 
-passwords = sys.argv[1:]
+passwords = map(lambda p: p.lower(), sys.argv[1:])
 
 if len(passwords) < 1:
     print("Too few arguments: %i" % len(passwords), file = sys.stderr)
     print("USAGE: %s PASSWORD PASSWORD..." % sys.argv[0])
     exit(1)
 
-len_ = len(passwords[0])
-for passw in passwords:
-    if len(passw) != len_:
+len_first = len(passwords[0])
+for password in passwords:
+    if len(password) != len_first:
         print("Passwords are of different length: '%s' and '%s'"
-                % (passwords[0], passw), file = sys.stderr)
-        exit(1)
+                % (passwords[0], password), file = sys.stderr)
+        exit(2)
+
+for each in passwords[:]:
+    line = raw_input("Enter password and number of correct letters\n> ")
+    (entered, correct) = tuple(re.split('\W+', line))
+    entered = entered.lower()
+    correct = int(correct)
+    if (entered not in passwords):
+        print("%s is not in the list of possible passwords" % entered, file = sys.stderr)
+        exit(3)
+    passwords.remove(entered)
+    for password in passwords:
+        if hamming(password, entered) != correct:
+            passwords.remove(password)
+
+    num = len(passwords)
+    if num > 1:
+        print("Possible passwords: " + ' '.join(passwords))
+    elif num == 1:
+        print("The password is: " + passwords[0])
+        exit(0)
+    else:
+        print("No more passwords left", file = sys.stderr)
+        exit(4)
