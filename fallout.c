ada24f64474d38d8c28d2c0cdc42dcda13fcdd4d
Styopa Semenukha <semenukha@gmail.com>
2015-12-13 03:24:44 -0500

    Print to stderr, read input

--- fallout.c
+++ fallout.c
@@ -4,9 +4,7 @@
 #include <ctype.h>
 
 void strtolower(char *ch) {
-	for (; *ch; ch++) {
-		*ch = tolower(*ch);
-	}
+	for (; *ch; ch++) *ch = tolower(*ch);
 }
 
 int hamming(const char *op1, const char *op2) {
@@ -24,7 +22,7 @@ int main(int argc, char **argv) {
 	int n_passw = argc - 1;
 
 	if (n_passw < 2) {
-		printf("Too few arguments: %i\nUSAGE: %s PASSWORD PASSWORD...\n",
+		fprintf(stderr, "Too few arguments: %i\nUSAGE: %s PASSWORD PASSWORD...\n",
 				n_passw, argv[0]);
 		return 1;
 	}
@@ -33,12 +31,20 @@ int main(int argc, char **argv) {
 	int len = strlen(passwords[0]);
 	for (i = 1; i < n_passw; i++) {
 		if (strlen(passwords[i]) != len) {
-			printf("Passwords are of different length: '%s' and '%s'\n",
+			fprintf(stderr, "Passwords are of different length: '%s' and '%s'\n",
 					passwords[0], passwords[i]);
 			return 2;
 		}
 	}
 
-	for (i = 1; i <= n_passw; i++) {
+	for (i = 0; i < n_passw; i++) strtolower(passwords[i]);
+
+	char *input;
+	int correct;
+	for (i = 0; i < n_passw; i++) {
+		printf("%s", "Enter password and number of correct letters\n> ");
+		if ( scanf("%ms %i", &input, &correct) != 2 ) return 0;
+		strtolower(input);
+		free(input);
 	}
 }
