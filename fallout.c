f79ebb8df0883a36c1fd2f65602911a9d06ea03a
Styopa Semenukha <semenukha@gmail.com>
2015-12-13 02:29:45 -0500

    Added strtolower, bugfixes

--- fallout.c
+++ fallout.c
@@ -1,5 +1,13 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void strtolower(char *ch) {
+	for (; *ch; ch++) {
+		*ch = tolower(*ch);
+	}
+}
 
 int hamming(const char *op1, const char *op2) {
 	int len = strlen(op1);
@@ -12,18 +20,25 @@ int hamming(const char *op1, const char *op2) {
 }
 
 int main(int argc, char **argv) {
-	if (argc < 3) {
+	int i;
+	int n_passw = argc - 1;
+
+	if (n_passw < 2) {
 		printf("Too few arguments: %i\nUSAGE: %s PASSWORD PASSWORD...\n",
-				argc - 1, argv[0]);
+				n_passw, argv[0]);
 		return 1;
 	}
 
-	int len = strlen(argv[1]);
-	for (int i = 2; i < argc; i++) {
-		if (strlen(argv[i]) != len) {
+	char **passwords = &argv[1];
+	int len = strlen(passwords[0]);
+	for (i = 1; i < n_passw; i++) {
+		if (strlen(passwords[i]) != len) {
 			printf("Passwords are of different length: '%s' and '%s'\n",
-					argv[1], argv[i]);
+					passwords[0], passwords[i]);
 			return 2;
 		}
+	}
+
+	for (i = 1; i <= n_passw; i++) {
 	}
 }
