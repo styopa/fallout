97a8764cb99e6ad6e0003a4aa20be87b16b45f04
Styopa Semenukha <semenukha@gmail.com>
2018-02-06 23:32:00 -0500

    Check input sanity

--- fallout.php
+++ fallout.php
@@ -7,10 +7,22 @@ function hamming($op1, $op2) {
     return count($ar1) - count($diff);
 }
 
+if ($argc < 4) {
+    $msg = 'Too few arguments: ' . ($argc - 1) . "\nUSAGE: ${argv[0]} PASSWORD PASSWORD...\n";
+    fwrite(STDERR, $msg);
+    exit(1);
+}
+
 $passwords = array_slice($argv, 1);
+$len = strlen($passwords[0]);
 array_walk(
     $passwords,
-    function (&$value, $key) {
+    function (&$value, $key) use ($len, $passwords) {
+        if (strlen($value) != $len) {
+            $msg = "Passwords are of different length: '$value' and '${passwords[0]}'\n";
+            fwrite(STDERR, $msg);
+            exit(2);
+        }
         $value = strtolower($value);
     }
 );
