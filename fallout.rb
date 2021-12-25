a05f5cd5ce19314b81b1c191fba815ae904a58d6
Styopa Semenukha <semenukha@gmail.com>
2021-12-25 10:03:36 -0500

    Verify password length

--- fallout.rb
+++ fallout.rb
@@ -2,10 +2,21 @@
 def hamming(op1, op2)
 end
 
-passwords = ARGV.map { |p| p.downcase }
+passwords = ARGV.map { |password| password.downcase }
 
 if passwords.length < 1
-  $stderr.puts "Too few arguments: #{passwords.length}"
-  $stderr.puts "USAGE: #$0 PASSWORD PASSWORD..."
+  $stderr.puts <<~EOF
+    Too few arguments: #{passwords.length}
+    USAGE: #$0 PASSWORD PASSWORD...
+  EOF
   exit 1
+end
+
+len_first = passwords[0].length
+passwords.each do |password|
+  if password.length != len_first
+    $stderr.puts "Passwords are of different length: '%s' and '%s'" \
+      % [passwords[0], password]
+    exit 2
+  end
 end
